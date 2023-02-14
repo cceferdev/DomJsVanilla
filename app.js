6064842ceb98ac9cc1099ede4233122ead974a01
CCEFERDEV <cefer.dev@gmail.com>
2023-02-14 12:11:17 -0400

    cambiosminimos

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ console.log(document.getElementById('parrafo'))
 
 const parrafo= document.querySelector('#parrafo')
 parrafo.textContent= 'texto desde js'
-/*para pintar con hmtl y modificr etiquetas */parrafo.innerHTML = '<b>texto con inner y en negrita</b>'
-parrafo.classList.add('h3.danger')
+/*para pintar con hmtl y modificr etiquetas parrafo.innerHTML = '<b>texto con inner y en negrita</b>'*/
+
+parrafo.classList.add('h3.danger', 'my-2')
  
