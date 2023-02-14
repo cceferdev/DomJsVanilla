5014ee6116aaddb726888b45c5d9506d44c820f6
CCEFERDEV <cefer.dev@gmail.com>
2023-02-14 11:53:27 -0400

    cambiosbreves

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-document.querySelector('h3')
+/*document.querySelector('h3')
 console.log(document.querySelector('h3'))
+console.log(document.querySelector('.h3-danger'))
+console.log(document.getElementById('parrafo'))
+*/
+
+const parrafo= document.querySelector('#parrafo')
+parrafo.textContent= 'texto desde js'
+/*para pintar con hmtl y modificr etiquetas */parrafo.innerHTML = '<b>texto con inner y en negrita</b>'
+parrafo.classList.add('h3.danger')
+ 
